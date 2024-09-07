e82724321a419ade386bb6e5c1cf8f6df3a06b2c
Mr. Python <2789762371@qq.com>
2024-09-07 17:07:03 +0800

    release 0.0.2

--- src/hastBilibili.js
+++ src/hastBilibili.js
@@ -1,61 +1,60 @@
-//@ts-check
 import { visit } from "unist-util-visit";
 import parsePath from "parse-path";
 
 export default function hastBilibili() {
   return (/** @type {import('hast').Root} */ tree) =>
-    visit(tree, "element", function (element) {
-      if (element.tagName !== "img" || !element.properties) return;
-      const src = element.properties.src;
-      if (typeof src !== "string") return;
-      if (!src.startsWith("bilibili:")) return;
-      const parsedUrl = parsePath(src);
-      if (/** @type {string} */ (parsedUrl.protocol) !== "bilibili") return;
-      const query = parsedUrl.query;
-      const r = {
-        danmaku: "0",
-        autoplay: "0",
-        playlist: "0",
-        high_quality: "1",
-      };
-      const pathname = parsedUrl.pathname;
-      const match = pathname.match(/^(av)?(\d+)$/);
-      if (match) r.aid = match[2];
-      else if (pathname.toLowerCase().startsWith("bv")) r.bvid = pathname;
-      else r.bvid = "bv" + pathname;
-
-      const page = Number(query.t || "");
-      if (page) r.page = String(page);
-
-      element.tagName = "div";
-      element.properties.style = "position: relative; padding-bottom: 62.5%";
-
-      element.children = [
-        {
-          type: "element",
-          tagName: "iframe",
-          properties: {
-            src:
-              "https://player.bilibili.com/player.html?" +
-              Object.entries(r)
-                .filter(([_, value]) => value !== undefined)
-                .map(
-                  ([key, value]) =>
-                    `${encodeURIComponent(String(key))}=${encodeURIComponent(
-                      String(value)
-                    )}`
-                )
-                .join("&"),
-            scrolling: "no",
-            border: 0,
-            frameborder: "no",
-            framespacing: 0,
-            allowfullscreen: true,
-            style:
-              "position: absolute; top: 0; left: 0; width: 100%; height: 100%;",
-          },
-          children: [],
+    visit(tree, "element", function (e) {
+      if ("img" !== e.tagName || !e.properties) return;
+      const t = e.properties.src;
+      if ("string" != typeof t) return;
+      if (!t.startsWith("bilibili:")) return;
+      const parsedPath = parsePath(t);
+      if ("bilibili" !== parsedPath.protocol) return;
+      const query = parsedPath.query;
+      const args = {
+          danmaku: "0",
+          autoplay: "0",
+          playlist: "0",
+          high_quality: "1",
         },
-      ];
+        pathname = parsedPath.pathname;
+      const aidMatch = pathname.match(/^(av)?(\d+)$/);
+      let page, time;
+      aidMatch
+        ? (args.aid = aidMatch[2])
+        : "bv" !== pathname.substring(0, 2).toLowerCase()
+        ? (args.bvid = "BV" + pathname)
+        : (args.bvid = pathname);
+      (page = Number(query.page || "")) && (args.page = String(page));
+      (time = Number(query.t || "")) && (args.t = String(time));
+      (e.tagName = "div"),
+        (e.properties.style = "position: relative; padding-bottom: 62.5%"),
+        (e.children = [
+          {
+            type: "element",
+            tagName: "iframe",
+            properties: {
+              src: "https://player.bilibili.com/player.html?".concat(
+                Object.entries(args)
+                  .filter((s) => s[1] !== undefined)
+                  .map((e) =>
+                    e
+                      .map((e) => encodeURIComponent(String(e)))
+                      .join("=")
+                      .replace(/=$/, "")
+                  )
+                  .join("&")
+              ),
+              scrolling: "no",
+              border: 0,
+              frameborder: "no",
+              framespacing: 0,
+              allowfullscreen: !0,
+              style:
+                "position: absolute; top: 0; left: 0; width: 100%; height: 100%;",
+            },
+            children: [],
+          },
+        ]);
     });
 }
